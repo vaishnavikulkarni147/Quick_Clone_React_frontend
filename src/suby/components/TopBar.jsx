fb5eae8ca671ff40a9a7983059c985687ade220a
Vaishnavi <vaishnavikulkarni714@gmail.com>
2025-02-22 16:41:14 +0530

    adding all files

---
+++ src/suby/components/TopBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const TopBar = () => {
+  return (
+   <section className="topBarSection">
+        <div className="companyTitle">
+            <Link to='/' className='link'>
+            <h2>Food express</h2>
+        
+            </Link>
+        </div>
+        <div className="searchBar">
+            <input type="text" placeholder='Search...' />
+        </div>
+        <div className="userAuth">
+            Login / SignUp
+        </div>
+   </section>
+  )
+}
+
+export default TopBar
